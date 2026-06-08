1894be09aa5433f9876ec176680d061e8a239ea5
DANIELA GELLOIO <danigeliodias@hotmail.com>
2026-06-08 14:15:24 +0100

    adicionando musica

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const botaoIniciar = document.querySelector(".app__card-primary-button");
 const displayTempo = document.querySelector("#timer");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+//readFile() é um metodo
+musica.loop(true) //musica fica em  loop
 
 const duracaoFoco = 1500;
 const duracaoDescansoCurto = 300;
@@ -32,19 +37,35 @@ longoBt.addEventListener("click", () => {
 
 //Apos refatorar, como temos o mesmo nome foco pra foto e mudando de cor podemos fazer assim
 
+musicaFocoInput.addEventListener('change', () =>{ //funcao anonima
+  //change é um eventopara trabalhar input true ou false
+  if (musica.paused){//se musica tiver pausada, da play 
+    musica.play()
+  }else{//play e pause sao metodos
+    musica.pause() //se nao pausa
+  }
+})
+
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
+  focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener("click", () => {
   alterarContexto("descanso-curto");
+  curtoBt.classList.add('active')
 });
 
 longoBt.addEventListener("click", () => {
   alterarContexto("descanso-longo");
+  longoBt.classList.add('active')
 });
 
 function alterarContexto(contexto) {
+  botoes.forEach(function (contexto){
+    //O forEach() serve para percorrer todos os elementos de uma lista, executando uma função para cada um deles.
+    contexto.classList.remove("active");
+  })
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `./imagens/${contexto}.png`);
   switch (contexto) {
